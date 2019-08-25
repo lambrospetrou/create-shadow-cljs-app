5003393260cfd8b2a62c2b050b12175934ded99d
Lambros Petrou <lambrospetrou@gmail.com>
2019-08-25 23:18:39 +0100

    Remove `yargs` and add `command-line-args`

--- src/cscljs/main.cljs
+++ src/cscljs/main.cljs
@@ -1,50 +1,77 @@
 (ns cscljs.main
   (:require 
     [clojure.string :as str]
+    [cljs.nodejs :as nodejs]
     ["colors" :as colors]
     ["path" :as path]
-    ["yargs" :as yargs]
+    ["command-line-args" :as cla]
+    ["command-line-usage" :as clu]
     ["shelljs" :as sh]))
-
-(defn read-args [^js yargs]
-  (-> (.options yargs (clj->js {
-    :n {
-      :alias "name",
-      :describe "The name of the project",
-      :type "string"
-    },
-    :d {
-      :alias "description",
-      :describe "The description of the project",
-      :type "string"
-    },
-    :i {
-      :alias "install",
-      :describe "If `true` it runs `npm install`. Use `--no-install` to skip.",
-      :default true,
-      :type "boolean"
-    }
-  }))
-  (.help)
-  (.-argv)))
 
 (defn __dirname [] (js* "__dirname"))
 
-(defn make-ctx [^js argv ^js sh]
-  (-> {
-        ; FIXME Convert `make-ctx` to receive a Clojure map instead of JS
-        :name (or (.-name argv) (aget (.-_ argv) 0) "")
-        :description (.-description argv)
-
-        :argv (js->clj argv)
-        :cwd (.. sh pwd toString)
-        :templatesPath (.join path (__dirname) "templates")
-
-        :sh sh
+(defn read-args [^js cla ^js clu ^js sh]
+  (let [
+    optionDefs 
+    [
+      {
+        :name "name"
+        :alias "n"
+        :description "The name of the project"
+        :defaultOption true
+        :defaultValue nil
+        :type js/String
+      },
+      {
+        :name "description"
+        :alias "d"
+        :description "The description of the project"
+        :defaultValue nil
+        :type js/String
+      },
+      {
+        :name "no-install"
+        :description "Skip `npm install` on the newly created project"
+        :defaultValue false
+        :type js/Boolean
       }
-      ((fn [m] (assoc m :projectPath (.join path (.. sh pwd toString) (:name m)))))))
+      {
+        :name "help"
+        :alias "h"
+        :description "Show usage"
+        :defaultValue false
+        :type js/Boolean
+      }
+    ]
+    options 
+    (js->clj (cla (clj->js optionDefs) (clj->js {:partial true})) :keywordize-keys true)]
+    (if (:help options)
+      (do
+        (println 
+          (clu (clj->js [
+            {:header "Typical Example",
+            :content "npx create-shadow-cls-app <app-name>"}
+            {:header "Available options"
+            :optionList optionDefs}
+            {:content "Project home: {underline https://github.com/lambrospetrou/create-shadow-cljs-app}"}
+          ])))
+          (.exit sh 1))
+      options)))
 
-(defn initProjectDir [{:keys [^js sh, name]}]
+(defn make-ctx [args ^js sh]
+  {
+    :name (:name args)
+    :description (:description args)
+
+    :args args
+    :cwd (.. sh pwd toString)
+    :templatesPath (.join path (__dirname) "templates")
+    :projectPath (.join path (.. sh pwd toString) (get args :name ""))
+
+    :sh sh
+  })
+
+(defn initProjectDir [{:keys [^js sh name]}]
   (when (str/blank? name)
     (.echo sh (.bgRed colors (.white colors "The project name cannot be empty. Provide one using the -n/--name options.")))
     (.exit sh 1))
@@ -55,22 +82,22 @@
   
   (.mkdir sh "-p" name))
 
-(defn copyTemplates [{:keys [^js sh, templatesPath, projectPath]}]
+(defn copyTemplates [{:keys [^js sh templatesPath projectPath]}]
   (.echo sh (.bold colors "\t:: Copying project files..."))
   (.cp sh "-rf" (.join path templatesPath "*") projectPath)
   (..
     sh
     (ShellString. (str/join "\n" [
-      "build/",
-      "node_modules/",
-      "target/",
-      "/yarn.lock",
-      ".shadow-cljs/",
-      ".nrepl-port",
+      "build/"
+      "node_modules/"
+      "target/"
+      "/yarn.lock"
+      ".shadow-cljs/"
+      ".nrepl-port"
     ]))
     (to (.join path projectPath ".gitignore"))))
 
-(defn updatePackageJson [{:keys [^js sh, name, description, projectPath]}]
+(defn updatePackageJson [{:keys [^js sh name description projectPath]}]
   (.echo sh (.bold colors "\t:: Updating `package.json`..."))
   (let [projectPkgJson (.join path projectPath "package.json")
         original (js->clj (.parse js/JSON (.. sh (cat projectPkgJson) (toString))))
@@ -81,14 +108,14 @@
                     (or description (get original "description"))))]
     (.. sh (ShellString. (.stringify js/JSON (clj->js updated) nil 2)) (to projectPkgJson))))
 
-(defn installDependencies [{:keys [^js sh, ^js argv, cwd, projectPath]}]
-  (when (get argv "install" false)
+(defn installDependencies [{:keys [^js sh args cwd projectPath]}]
+  (when (not (:no-install args))
     (.echo sh (.bold colors "\t:: Installing NPM dependencies..."))
     (.cd sh projectPath)
     (.exec sh "npm install")
     (.cd sh cwd)))
 
-(defn initGitRepository [{:keys [^js sh, cwd, projectPath]}]
+(defn initGitRepository [{:keys [^js sh cwd projectPath]}]
   (when (.which sh "git")
     (.echo sh (.bold colors "\t:: Initializing .git..."))
     (.cd sh projectPath)
@@ -98,8 +125,8 @@
     (.cd sh cwd)))
 
 (defn -main []
-  (let [argv (read-args ^js yargs)
-        ctx (make-ctx argv ^js sh)]
+  (let [args (read-args cla clu sh)
+        ctx (make-ctx args sh)]
     (set! (-> sh (.-config) (.-silent)) true)
     (set! (-> sh (.-config) (.-fatal)) true)
 
