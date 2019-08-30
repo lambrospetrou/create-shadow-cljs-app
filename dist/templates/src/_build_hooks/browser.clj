574d3686b645e5eb35e7aa31819f2ae9e00c6a52
Lambros Petrou <lambrospetrou@gmail.com>
2019-08-30 21:49:04 +0100

    Update .gitignore to not contain the output directory

--- dist/templates/src/_build_hooks/browser.clj
+++
@@ -1,8 +0,0 @@
-(ns _build_hooks.browser
-  (:require [clojure.java.shell :as sh]))
-
-(defn copy-index
-  {:shadow.build/stage :flush}
-  [state & args]
-  (sh/sh "cp" "src/_build_hooks/index.html" "build/browser")
-  state)
